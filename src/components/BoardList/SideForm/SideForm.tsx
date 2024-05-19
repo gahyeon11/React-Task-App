9282c0faa25174add5e1f588f66cc370a91b25aa
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-05-19 18:15:18 +0900

    docs: 0516, boarderList, sideForm, List Container, List, Task, Action Button, DropDownForm 생성

--- src/components/BoardList/SideForm/SideForm.tsx
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
+import { FiCheck } from 'react-icons/fi';
 
-const SideForm = () => {
+type TSideFormProps = {
+  setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  inputRef: React.RefObject<HTMLInputElement>;
+}
+const SideForm: FC<TSideFormProps> = ({
+  setIsFormOpen,
+  inputRef
+}) => {
+  const [inputText, setInputText] = useState('');
+
+  const hadleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  }
+  const handleOnBlur = ()=>{
+    setIsFormOpen(false);
+  }
   return (
-    <div>SideForm</div>
+    <div>
+      <input type="text" 
+      // ref = {inputRef}
+      autoFocus
+      placeholder='새로운 게시판 등록하기' 
+      value={inputText}
+      onChange={hadleChange}
+      onBlur={handleOnBlur}
+      />
+      <FiCheck/>
+    </div>
   )
 }
 
